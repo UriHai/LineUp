2259f76f1708149a8aeb8a780b12db547c81cff6
uri <13288uri@gmail.com>
2025-06-23 13:22:34 +0300

    Question 2

--- LineUp/Main.cpp
+++ LineUp/Main.cpp
@@ -1,9 +1,12 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 int main() {
     // Question 1
-    vector<int> v = { 5, 4, 3, 2, 1 };
+    vector<int> v = { 5, 4, 3, 2, 1, 4, 4 };
     
+    //Question 2
+    cout << count(v.begin(), v.end(), 4);
     return 0;
 }
